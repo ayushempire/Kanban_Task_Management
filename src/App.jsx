3f1701b7d9f0a683e417b9ddd0b77926efa2615f
Ayush <ayushempire03@gmail.com>
2023-08-28 17:55:36 +0530

    initialize commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./Components/Home";
+import Navbar from "./Screens/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
