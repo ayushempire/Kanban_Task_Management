3bf2acc066dc407cb679c7b2d822f92fda4ca46a
Ayush <ayushempire03@gmail.com>
2023-08-28 20:07:39 +0530

    connected to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
 // importing express from express
 import express from "express";
 
+// importing mongoose
+import mongoose from "mongoose";
+
 // creating instance of express to app
 const app = express();
 
 // selecting port on that we want to run bakend
 const port = 5000;
+
+// mongodb url
+const mongoURL =
+  "mongodb+srv://ayushempire03:kanbanapp@kanbanapp.ympxhwn.mongodb.net/kanban_appretryWrites=true&w=majority";
+
+// mongodb connection
+await mongoose
+  .connect(mongoURL, { useNewUrlParser: true })
+
+  .then(() => {
+    console.log("connected successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // app.get method
 app.get("/", (req, res) => {
