47295a64f308a8df31731252f387537ddfa6a98c
Ayush <ayushempire03@gmail.com>
2023-08-29 18:48:10 +0530

    made home page, and styled it , not sign feilfd

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,44 +1,45 @@
 import React from "react";
 import kmslogo from "../kmslogo.svg";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div>
-      <footer class="d-flex bg-dark flex-wrap justify-content-start align-items-center py-3 px-3 border-top">
-        <p class="col-md-4 mb-0 text-white fs-5">© 2023 KMS, Limited</p>
+      <footer className="d-flex bg-dark flex-wrap justify-content-start align-items-center py-3 px-3 border-top">
+        <p className="col-md-4 mb-0 text-white fs-5">© 2023 KMS, Limited</p>
 
-        <a
-          href="/"
-          class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+        <Link
+          to="/"
+          className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
         >
-          <img class="bi me-2" src={kmslogo} width="170" height="50"></img>
-        </a>
+          <img className="bi me-2" src={kmslogo} width="170" height="50"></img>
+        </Link>
 
-        <ul class="nav col-md-4 justify-content-end">
-          <li class="nav-item">
-            <a href="#" class="nav-link px-2 text-white fw-bold">
+        <ul className="nav col-md-4 justify-content-end">
+          <li className="nav-item">
+            <Link to="#" className="nav-link px-2 text-white fw-bold">
               Privacy Pilicy
-            </a>
+            </Link>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link px-2 text-white fw-bold">
+          <li className="nav-item">
+            <Link to="#" className="nav-link px-2 text-white fw-bold">
               Features
-            </a>
+            </Link>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link px-2 text-white fw-bold">
+          <li className="nav-item">
+            <Link to="#" className="nav-link px-2 text-white fw-bold">
               Pricing
-            </a>
+            </Link>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link px-2 text-white fw-bold">
+          <li className="nav-item">
+            <Link to="#" className="nav-link px-2 text-white fw-bold">
               Contact us
-            </a>
+            </Link>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link px-2 text-white fw-bold">
+          <li className="nav-item">
+            <Link to="#" className="nav-link px-2 text-white fw-bold">
               About
-            </a>
+            </Link>
           </li>
         </ul>
       </footer>
--- src/Screens/HomePage.jsx
+++ src/Screens/HomePage.jsx
@@ -1,12 +1,58 @@
 import React from "react";
+import "./HomePage.css";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
-  // style for homepage
-  //   const styleHome = {};
+  // page navigator
+  const navigate = useNavigate();
+
+  // handle signup funcion
+  const handleSignup = () => {
+    navigate("/Login");
+  };
 
   return (
-    <div className="homePage" style={{ minHeight: "100vh" }}>
-      HomePage
+    <div
+      className=" homePage d-flex flex-column flex-md-column flex-lg-row"
+      style={{ minHeight: "80vh" }}
+    >
+      <div
+        className=" container text-white pt-5 me-5 ps-lg-5"
+        style={{ zIndex: 2 }}
+      >
+        <h1 className="ms-lg-4" style={{ fontSize: "45px" }}>
+          KMS makes it easier for peoples to manage projects and tasks
+        </h1>
+        <p className="pt-4 fs-3 ms-lg-4">
+          Simple, flexible, and powerful. All it takes are boards, lists, and
+          cards to get a clear view of who’s doing what and what needs to get
+          done
+        </p>
+        <h5 className="pt-4 ms-lg-4">WHAT YOU GET ON THE FREE PLAN:</h5>
+        <ul className="fs-5 mt-3 ms-lg-5">
+          <li>Unlimited cards</li>
+          <li>Unlimited Power-Ups per board</li>
+        </ul>
+      </div>
+      <div className="container pt-5  ms-lg-5 mb-5 mb-lg-0">
+        <form
+          className="d-flex flex-column flex-md-column flex-lg-column rd"
+          onSubmit={handleSignup}
+        >
+          <input
+            className="mt-2 mb-3 ps-2 "
+            type="search"
+            placeholder="Search"
+          />
+          <button
+            className="btn btn-outline-success"
+            type="submit"
+            style={{ width: "100px" }}
+          >
+            sign up
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
