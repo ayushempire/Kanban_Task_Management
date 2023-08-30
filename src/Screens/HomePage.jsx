b51a340aac22ce25cc82c84d697c44f7ad45bf32
Ayush <ayushempire03@gmail.com>
2023-08-30 21:04:04 +0530

    corrected the homepage js post method

--- src/Screens/HomePage.jsx
+++ src/Screens/HomePage.jsx
@@ -28,7 +28,7 @@ export default function HomePage() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        eamil: signup.email,
+        email: signup.email,
         password: signup.password,
       }),
     });
