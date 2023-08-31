caf6db0086d4dc131d79fe2af9f3812d12ba249b
Ayush <ayushempire03@gmail.com>
2023-09-01 00:33:37 +0530

    added logout button at board navbar

--- src/KANBOARD/BoardNav.jsx
+++ src/KANBOARD/BoardNav.jsx
@@ -6,10 +6,36 @@ import kmslogo from "../kmslogo.svg";
 export default function BoardNav() {
   return (
     <Navbar>
-      <div className="container-fluid">
-        <Link className="ps-1 py-2">
-          <img src={kmslogo} width="180" height="45" />
-        </Link>
+      <div className="navbar navbar-expand-lg">
+        <div className="container-fluid d-flex ">
+          <Link className=" py-2 ">
+            <img src={kmslogo} width="180" height="45" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse position-relative "
+            id="navbarSupportedContent"
+          >
+            <div className="align-content-center d-flex align-items-center ms-auto">
+              <button
+                className="btn btn-outline-warning bg-warning text-dark fw-bold border-dark "
+                type="button"
+              >
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </Navbar>
   );
